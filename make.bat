d23812cccc433dea4dbb1788ff01153ebb6ce462
phantomix-github <phantomix@macht-albern.de>
2017-06-22 00:05:12 +0200

    170621PX Added protocol-drafts and protocol to windows makefile; changed vorstandssitzung status to PUBLIC

--- make.bat
+++ make.bat
@@ -2,6 +2,8 @@
 @if not exist "_build" mkdir "_build"
 @if not exist "pdf" mkdir "pdf"
 @if not exist "pdf\doorlock" mkdir "pdf\doorlock"
+@if not exist "pdf\protocol-drafts" mkdir "pdf\protocol-drafts"
+@if not exist "pdf\protocol" mkdir "pdf\protocol"
 cd _build
 
 
@@ -13,6 +15,16 @@ cd _build
     for %%f in (..\doorlock\*.tex) do (
         pdflatex ..\doorlock\%%~nf.tex
         move %%~nf.pdf ..\pdf\doorlock
+    )
+
+    for %%f in (..\protocol-drafts\*.tex) do (
+        pdflatex ..\protocol-drafts\%%~nf.tex
+        move %%~nf.pdf ..\pdf\protocol-drafts
+    )
+
+    for %%f in (..\protocol\*.tex) do (
+        pdflatex ..\protocol\%%~nf.tex
+        move %%~nf.pdf ..\pdf\protocol
     )
 
 cd ..
